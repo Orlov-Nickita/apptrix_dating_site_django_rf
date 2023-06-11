1b423184a5c63e2dbd7e54e0318a547fd6314f4e
Nikita Orlov <orlov.nickita@gmail.com>
2023-06-12 00:21:43 +0300

    added some specs for hosting

--- apptrix/apptrix/settings.py
+++ apptrix/apptrix/settings.py
@@ -142,7 +142,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'api_clients/static/'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- apptrix/apptrix/wsgi.py
+++ apptrix/apptrix/wsgi.py
@@ -1,16 +1,17 @@
-"""
-WSGI config for apptrix project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
+# -*- coding: utf-8 -*-
+import platform
+import environ
+import sys
 import os
 
+env = environ.Env()
+environ.Env.read_env()
+
+sys.path.insert(0, '/home/c/cq67819/django/public_html/apptrix')
+sys.path.insert(0, '/home/c/cq67819/django/public_html/apptrix/apptrix')
+sys.path.insert(0, '/home/c/cq67819/django/venv/lib/python{0}/site-packages'.format(platform.python_version()[0:3]))
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "apptrix.settings"
+
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apptrix.settings')
-
 application = get_wsgi_application()
